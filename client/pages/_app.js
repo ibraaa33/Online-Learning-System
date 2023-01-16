8a4b51b1e0392fac1bf7aea68ef7e838bbe651c5
Ibra <ibra@MacBook-Air-Ibra.local>
2023-01-17 00:10:25 +0100

    Added login

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -4,14 +4,17 @@ import "../public/css/styles.css";
 import TopNav from "../components/TopNav";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { Provider } from "../context";
 
 function MyApp({ Component, pageProps }) {
     
   return(
     <>
+    <Provider>
     <ToastContainer position="top-center" />
     <TopNav />
     <Component {...pageProps} />
+    </Provider>
   </>
    );
 }
